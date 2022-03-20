aaa0def8d6c8ff7578931159be734430420be96e
Sensei <Sensei@THESENSEI>
2022-03-20 16:33:37 +0300

    Proje dosyası ekle.

---
+++ PageRouter/UserMaster.master.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class UserMaster : System.Web.UI.MasterPage
+{
+    SqlBaglanti sqlBaglan = new SqlBaglanti(); 
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        SqlCommand sqlCommand = new SqlCommand("select * from Tbl_Category",sqlBaglan.sqlBaglanti());
+        SqlDataReader reader = sqlCommand.ExecuteReader();
+        DataList1.DataSource = reader;
+        DataList1.DataBind();
+    }
+}
